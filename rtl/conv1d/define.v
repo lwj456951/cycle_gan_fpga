3b6b0529951a5759e510815084b4277283a112e4
lwj_ic <jia200151@126.com>
2025-10-30 14:20:44 +0800

    add rtl

---
+++ rtl/conv1d/define.v
@@ -0,0 +1,10 @@
+/*** 
+ * @Author: jia200151@126.com
+ * @Date: 2025-10-29 14:06:34
+ * @LastEditors: lwj
+ * @LastEditTime: 2025-10-29 17:11:01
+ * @FilePath: \conv1d\define.v
+ * @Description: 
+ * @Copyright (c) 2025 by lwj email: jia200151@126.com, All Rights Reserved.
+ */
+ `define WIDTH_DATA 16//support width_data = 8 16 
